d744670e33b10af900bb3ab573127aef8c3ad577
ali reza <lordiroh3@gmail.com>
2024-01-23 16:56:31 +0330

    new Scripts and add Icons

--- SaveData/Scripts/AutoSave/AutoSaveEntities.cs
+++ SaveData/Scripts/AutoSave/AutoSaveEntities.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class AutoSaveEntities : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
